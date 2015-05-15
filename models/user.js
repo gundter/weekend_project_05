3edbb3197c0460a3eb86b02ba1d69622a3803ef6
Terry Gunderson <tgunderson25@gmail.com>
2015-05-15 18:08:47 -0500

    Normal mode: adds first name, last name, and email to register form

--- models/user.js
+++ models/user.js
@@ -5,7 +5,10 @@ var mongoose = require('mongoose'),
 
 var UserSchema = new Schema({
     username: { type: String, required: true, index: { unique: true} },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    email: String
 });
 
 UserSchema.pre('save', function(next){
